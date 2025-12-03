f8b620ed4a073031adc527bf0b8a618588855a46
Tojinazarovbekzod <bekzodtojinazar@gmail.com>
2025-12-03 13:43:43 +0500

    fix

--- foydalanuvchi.py
+++ foydalanuvchi.py
@@ -52,28 +52,3 @@ class Menu(Data):
         except ValueError:
             print("Faqat raqam kiriting")
         
-# class Menu(Data):
-#     def start(self):
-#         while True:
-#             print("""
-#             1 - Bo'sh xonalar
-#             2 - Bron qilish
-#             3 - Mening bronlarim
-#             4 - Profilim
-#             0 - Chiqish
-#             """)
-#             try:
-#                 tanlov = int(input("Tanlang: "))
-
-#                 if tanlov == 1:
-#                     print("Bo'sh xonalar:")
-#                     print(self.bosh_xonalar())
-
-#                 elif tanlov == 0:
-#                     print("Chiqdingiz")
-#                     break
-
-#                 else:
-#                     print("Nato'g'ri tanlov! ")
-#             except ValueError:
-#                 print("Faqat raqam kiriting")
