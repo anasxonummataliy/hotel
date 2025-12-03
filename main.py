d437eea9cbcff2a85c15bd1a27491f8d38e3ecfe
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-12-03 15:26:59 +0500

    fix

--- main.py
+++ main.py
@@ -6,17 +6,27 @@ from user import user
 
 
 def start_project():
-    draw_box("PDP Hotel")
-    print()
-    print(colored('1. Mijoz\n2. Admin\n3. Chiqish\n', 'yellow'))
-    choice = input(colored("Tanlov >>> ", 'magenta'))
-    match choice:
-        case '1':
-            user()
-        case '2':
-            admin()
-        case '3':
-            exit()
+    while True:
+        while True:
+            draw_box("PDP Hotel")
+            print()
+            print(colored("1. Mijoz\n2. Admin\n3. Chiqish\n", "yellow"))
+            choice = input(colored("Tanlov >>> ", "magenta"))
+            match choice:
+                case "1":
+                    clear_console()
+                    user()
+                case "2":
+                    clear_console()
+                    admin()
+                case "3":
+                    exit()
+                case _:
+                    clear_console()
+                    print(colored("Xato ma'lumot kiritdingiz❗️ \nQayta kiriting.", "red"))
+                    input(colored("Davom etish uchun ENTERni bosing...", "green"))
+                    clear_console()
+                    break
 
 
 start_project()
