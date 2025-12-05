82b94a7fd199a5b50055e10f2fc9bbc8782909d7
Tojinazarovbekzod <bekzodtojinazar@gmail.com>
2025-12-05 11:23:02 +0500

    fix

--- main.py
+++ main.py
@@ -1,28 +1,85 @@
-from register import *
-from login import *
-from admin import *
-def main():
-    while True:
-        print("""
-        1 - Ro'yxatdan o'tish
-        2 - Tizimga kirish
-        3 - Admin
-        """)
+import json
+
+class User:
+    File_user = 'data/user.json'
+
+    def __init__(self,name, familiya, telefon, login, parol):
+        self.name= name
+        self.familiya = familiya
+        self.telefon = telefon
+        self.__login = login
+        self.__parol = parol
+
+
+
+    def get_login(self):
+        return self.__login
+
+    def get_parol(self):
+        return self.__parol
+
+    def ism(self):
+        return self.name
+
+    def get_familiya(self):
+        return self.familiya
+
+    def phone(self):
+        return self.telefon
+
+    def set_parol(self, new_pass):
+        self.__parol = new_pass
+
+    def to_dict(self):
+        return {
+            'ism': self.name,
+            'familiya': self.familiya,
+            'telefon': self.telefon,
+            'login': self.__login,
+            'parol':self.__parol
+        }
+    @staticmethod
+    def from_dict(data):
+        return User(
+            data['ism'],
+            data['familiya'],
+            data['telefon'],
+            data['login'],
+            data['parol']
+        )
+
+
+
+class UserDatabase:
+    File_user = 'data/user.json'
+
+    def __init__(self):
+        self.users = self.load_users()
+
+    def load_users(self):
         try:
-            tanlov = input("Tanlovingizni kiriting: ")
-            if tanlov == "1":
-                register()
-            elif tanlov == "2":
-                login()
-            elif tanlov == "3":
-                admin()
-            elif tanlov == "0":
-                print("Dasturdan chiqdingiz")
-                break
-            else:
-                print("Notogri tanlov")
-        except ValueError:
-            print("Faqat raqam kiriting")
-            
-if __name__ == "__main__":
-    main()
+            with open(User.File_user, 'r')as f:
+                data = json.load(f)
+                return [User.from_dict(u) for u in data]
+        except (json.JSONDecodeError, FileNotFoundError):
+            return []
+
+    def save_data(self):
+        with open(User.File_user, 'w')as f:
+            json.dump([u.to_dict() for u in self.users], f, indent=4)
+
+    def user_exists(self, login):
+        for i in self.users:
+            if i.get_login() == login:
+                return i
+        return None
+
+    def add_user(self, user):
+        self.users.append(user)
+        self.save_data()
+
+    def get_user(self, login):
+        for u in self.users:
+            if u.get_login() == login:
+                return u
+        return None
