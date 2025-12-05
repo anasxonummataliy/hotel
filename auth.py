14c0d07d0805661b082f53b562b8e3ccec5a8964
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-12-05 18:03:58 +0500

    fix

--- auth.py
+++ auth.py
@@ -31,28 +31,57 @@ class User:
 
 
 def register():
-    users = get_data(FILE_PATH)
-    draw_box("Ro'yhatdan o'tish")
-    first_name = input(colored("Ism: ", "magenta"))
-    last_name = input(colored("Familiya: ", "magenta"))
-    phone = input(colored("Telefon: ", "magenta"))
-    login = input(colored("Login: ", "magenta"))
-    password = input(colored("Parol: ", "magenta"))
+    while True:
+        users = get_data(FILE_PATH)
+        draw_box("Ro'yhatdan o'tish")
+        first_name = input(colored("Ism >>> ", "magenta"))
+        last_name = input(colored("Familiya >>> ", "magenta"))
+        while True:
+            phone = input(colored("Telefon(exp: +998940581306) >>>", "magenta"))
+            if phone.startswith('+998') and len(phone) == 13:
+                break
+            else:
+                print(colored("Noto'g'ri formatda kiritdingiz.\nIltimos qayta kiriting❗️", 'red'))
 
-    new_user = User(first_name, last_name, phone, login, password)
-    user_id = get_new_id(users)
-    new_user_data = {
-        "id": user_id,
-        "first_name": new_user.first_name,
-        "last_name": new_user.last_name,
-        "phone": new_user.phone,
-        "login": new_user.login,
-        "password": new_user.get_password,
-    }
+        while True:
+            login = input(colored("Login >>> ", "magenta"))
+            flag = False
+            if len(login) < 6:
+                print(colored("❌ Login kamida 6 ta belgi bo'lishi kerak!", "red"))
+            else:
+                for user in users:
+                    if login == user["login"]:
+                        flag = True
+                if flag:
+                    print(
+                        colored(
+                            "Bunday foydalanuvchi mavjud, Iltimos boshqa login kiriting❗️",
+                            "red",
+                        )
+                    )
+                else:
+                    break
 
-    users.append(new_user_data)
-    save_data(FILE_PATH, users)
-    return user_id
+        while True:
+            password = input(colored("Parol >>> ", "magenta"))
+            if len(password) < 6:
+                print(colored("❌ Parol kamida 6 ta belgi bo'lishi kerak!", "red"))
+            else:
+                break
+
+        new_user = User(first_name, last_name, phone, login, password)
+        user_id = get_new_id(users)
+        new_user_data = {
+            "id": user_id,
+            "first_name": new_user.first_name,
+            "last_name": new_user.last_name,
+            "phone": new_user.phone,
+            "login": new_user.login,
+            "password": new_user.get_password,
+        }
+        users.append(new_user_data)
+        save_data(FILE_PATH, users)
+        return user_id
 
 
 def login():
